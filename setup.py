5cb5fef31d556289dad6614895340c0e63683944
Diprog <diprog991@gmail.com>
2023-10-24 23:11:13 +0300

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pw-cdtp-proxy',
-    version='0.1.2',
+    version='0.1.3',
     description='My Python package',
     author='Dmitry',
     author_email='diprog991@gmail.com',
